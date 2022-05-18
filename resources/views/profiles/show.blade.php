a9538fb7e56c9a69125b9a85bc65f2e5354bae88
Christina Phan <cphan4school@gmail.com>
2022-05-18 01:34:24 -0700

    Added associated Groups to display on Profile.show

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,3 +1,35 @@
+@php
+
+// For when there are duplicate groups -> appends number after name
+    // For the purpose of the assignment, the table does not take duplicate values
+    $groups = $profile->groups->sortBy('name');
+
+    $countGroups = $groups->countBy('name');
+
+    foreach($countGroups as $group => $counts) {
+        if ($counts === 1) {
+            unset($countGroups[$group]);
+        }
+    }
+
+    $i = 1;
+    $groups->each(function($group) use(&$i, $countGroups) {
+        if (isset($countGroups[$group->name])) {
+                $oldName = $group->name;
+                if ($i > 1) {
+                    $group->name = $group->name . " ($i)";
+                }
+                $i++;
+                if ($i > $countGroups[$oldName]) {
+                    $i = 1;
+                }
+        } else {
+            $i = 1;
+        }
+    });
+
+@endphp
+
 @extends('layout')
 
 @section('title', "$profile->first_name $profile->last_name")
@@ -23,8 +55,20 @@
             </ul>
         @else 
         <div class="fst-italic">Empty, for now. <span class="d-block">Check back later!</span></div>
+        @endif
     </div>
-    @endif
+    <div class="slide">
+        <div class="label h1 fw-bold">Groups</div>
+        @if($groups->count() > 0)
+            <ul class="list-unstyled d-flex flex-column align-items-center align-items-md-start">
+                @foreach($groups as $group)
+                    <li class="info my-1">{{ $group->name }}</li>
+                @endforeach
+            </ul>
+        @else 
+        <div class="fst-italic">{{ $profile->first_name }} {{ $profile->last_name }} hasn't joined any yet.<span class="d-block">Check back later!</span></div>
+        @endif
+    </div>
 </address>
 <div class="slide d-flex justify-content-center justify-content-md-start">
     <a class="btn" id="link-edit"
