0b6590a49af7c99c0519276bd3361036273cba0a
Christina Phan <cphan4school@gmail.com>
2022-06-14 15:23:24 -0700

    Added query functionality and changed profile layout to table

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -1,35 +1,53 @@
+@php
+    $sortBy = request()->get('sortBy');
+    $direction = request()->get('direction');
+@endphp
+
 @extends('dashboard')
 
 @section('title', 'Manage Profiles')
 
 @section('pages')
-{{ $profiles->links('vendor.pagination.custom')}}
+{{ $profiles->withQueryString()->links('vendor.pagination.custom')}}
 @endsection
 
 {{-- TODO: Add a mouseover/focus event that creates the bottom border even when tabbing (currently limited to hover) --}}
 @section('content')
-<ul class="text-center" id="profiles-container">
-    @foreach($profiles as $profile)
-        <li class="profile-wrapper text-center text-md-start px-auto py-4 my-1 ps-md-5" id="{{ $profile->id }}">
-            <p class="profile-name" tabindex="-1">{{ $profile->first_name }} {{ $profile->last_name }}</p>
-            <div class="canvas-div d-none">
-            <canvas class="canvas py-2" width="0" height="75px"></canvas>
-            </div>
-            <div class="profile-link-group">
-                <div class="profile-link-group-a d-flex justify-content-md-start justify-content-evenly">
-                    <a class="btn profile-edit-link profile-link link-a"
-                        href="{{ route('profiles.edit', $profile->id ) }}">Edit</a>
-                        <a class="btn profile-view-link profile-link link-a"
-                        href="{{ route('profiles.show', $profile->id) }}">View</a>
-                        <form class="profile-link profile-delete-link d-inline-block me-0" action="{{ route('profiles.destroy', $profile->id) }}" method="POST" onSubmit="return confirm('Are you sure you want to delete?');">
-                            @csrf
-                            @method('DELETE')
-                    <button class="w-100 btn profile-link link-a form-link-a"
-                        type="submit">Delete</button>
-                        </form>
-                </div>
-            </div>
-        </li>
-    @endforeach
-</ul>
+<div class="table-responsive bg-white">
+            <table class="table text-center">
+                <thead>
+                    <tr>
+                        <th scope="col">
+                            <div class="text-nowrap">
+                                <a href={{ route('profiles.index', ["sortBy" => "first_name", "direction" => $sortBy == "first_name" ? $direction == "asc" ? "desc" : "asc" : "asc"]) }} class="text-decoration-none">First Name</a>
+                            </div>
+                        </th>
+                        <th scope="col">
+                            <div class="text-nowrap">
+                                <a href={{ route('profiles.index', ["sortBy" => "last_name", "direction" => $sortBy == "last_name" ? $direction == "asc" ? "desc" : "asc" : "asc"]) }} class="text-decoration-none">Last Name</a>
+                            </div>
+                        </th>
+                        <th scope="col">
+                            <div class="text-nowrap">
+                                <a href={{ route('profiles.index', ["sortBy" => "email", "direction" => $sortBy == "email" ? $direction == "asc" ? "desc" : "asc" : "asc"]) }} class="text-decoration-none">Email</a>
+                            </div>
+                        </th>
+                        <th scope="col">
+                            <div class="text-nowrap">
+                                Phone Number
+                            </div>
+                        </th>
+                </thead>
+                <tbody>
+                    @foreach($profiles as $profile)
+                        <tr>
+                            <td class="align-middle">{{ $profile->first_name }}</td>
+                            <td class="align-middle">{{ $profile->last_name }}</td>
+                            <td class="align-middle">{{ $profile->email }}</td>
+                            <td class="align-middle">{{ $profile->phone_number }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
 @endsection
